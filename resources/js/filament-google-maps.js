90eb5a8c73ba2a45b5454f9ed2fbe17b6ecae17e
Thor <victorcampos@grupomezzana.com>
2025-05-23 12:48:39 +0200

    🎨 Agregar color de relleno a las figuras en el mapa.

--- resources/js/filament-google-maps.js
+++ resources/js/filament-google-maps.js
@@ -598,7 +598,7 @@ export default function filamentGoogleMapsField({
             Object.assign({}, this.polyOptions, {
               id: feature.properties.id,
               type: feature.properties.type,
-              // fillColor: feature.properties.color,
+              fillColor: feature.properties.color,
               bounds: new google.maps.LatLngBounds(SW, NE),
               editable: false,
             })
@@ -609,7 +609,7 @@ export default function filamentGoogleMapsField({
             Object.assign({}, this.polyOptions, {
               id: feature.properties.id,
               type: feature.properties.type,
-              // fillColor: feature.properties.color,
+              fillColor: feature.properties.color,
               paths: this.transformToMVCArray(feature.geometry.coordinates),
               editable: false,
             })
@@ -632,7 +632,7 @@ export default function filamentGoogleMapsField({
             Object.assign({}, this.polyOptions, {
               id: feature.properties.id,
               type: feature.properties.type,
-              // fillColor: feature.properties.color,
+              fillColor: feature.properties.color,
               center: new google.maps.LatLng(
                 feature.geometry.coordinates[1],
                 feature.geometry.coordinates[0]
